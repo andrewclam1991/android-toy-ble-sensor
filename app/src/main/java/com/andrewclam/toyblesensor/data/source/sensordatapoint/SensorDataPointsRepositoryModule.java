e241587d4f5d4c93fe314233a575f59796e12958
Andrew Lam <23492362+andrewclam1991@users.noreply.github.com>
2018-08-19 13:32:39 -0700

    add sensor <-> data points data implementation boilerplate
    Signed-off-by: Andrew Lam <23492362+andrewclam1991@users.noreply.github.com>

---
+++ app/src/main/java/com/andrewclam/toyblesensor/data/source/sensordatapoint/SensorDataPointsRepositoryModule.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2018 Andrew Chi Heng Lam
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * com.andrewclam.toyblesensor.data.source.sensordatapoint.SensorDataPointsRepositoryModule
+ */
+
+package com.andrewclam.toyblesensor.data.source.sensordatapoint;
+
+import android.support.annotation.NonNull;
+
+import com.andrewclam.toyblesensor.data.source.Local;
+import com.andrewclam.toyblesensor.data.source.Remote;
+import com.andrewclam.toyblesensor.data.source.Repo;
+
+import javax.inject.Singleton;
+
+import dagger.Binds;
+import dagger.Module;
+
+/**
+ * This is used by Dagger to inject the required arguments into the {@link SensorDataPointsRepository}.
+ */
+@Module
+public abstract class SensorDataPointsRepositoryModule {
+
+  @NonNull
+  @Singleton
+  @Binds
+  @Repo
+  abstract SensorDataPointsDataSource providesRepository(@NonNull SensorDataPointsRepository repository);
+
+  @NonNull
+  @Singleton
+  @Binds
+  @Local
+  abstract SensorDataPointsDataSource providesLocalDataSource(@NonNull SensorDataPointsLocalDataSource dataSource);
+
+  @NonNull
+  @Singleton
+  @Binds
+  @Remote
+  abstract SensorDataPointsDataSource providesRemoteDataSource(@NonNull SensorDataPointsLocalDataSource dataSource);
+
+}
